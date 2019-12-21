b7a6bd918411565f0eef37423a513734bed48374
Ngoc Truong <ngoctruong@teqnological.asia>
2019-12-21 20:48:17 +0700

    update middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express, { Application, Request, Response } from "express"
+import morgan from "morgan"
+import cors from "cors"
+import helmet from "helmet"
+import compression from "compression"
 
 const app: Application = express()
+
+/** Setup middlewares */
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 /**
  * demo routes
  */
 app.get("/health", (req: Request, res: Response): void => {
-    res.send("demo-service:OK")
+    res.send("OK!")
 })
 
 export default app
